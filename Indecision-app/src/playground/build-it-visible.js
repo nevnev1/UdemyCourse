377d5204e0bf7f501470f296fd823b13f2b34b33
Nevin Lenior <nevindlenior@gmail.com>
2019-02-18 13:59:27 +0100

    section 4 collage 37

--- Indecision-app/src/playground/build-it-visible.js
+++ Indecision-app/src/playground/build-it-visible.js
@@ -1,20 +1,41 @@
-let showOrHide = false;
-const onVisibility = () => {
-    showOrHide = !showOrHide;
-    render();
-};
-
-const appRoot = document.getElementById('app');
-const render = () => {
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={onVisibility}>{showOrHide ? 'Hide details': 'Show details'}</button>
-            {showOrHide && <p>Some details here.</p>}
-        </div>
-    );
-
-    ReactDOM.render(template, appRoot);
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visible: false,
+            visibleText: 'Show details',
+            hiddenText: false
+        };
+    }
+    handleToggleVisibility() {
+        if(this.state.visible) {
+            this.setState(() => {
+                return {
+                    visible: false,
+                    visibleText: 'Show details',
+                    hiddenText: false
+                }
+            });
+        } else {
+            this.setState(() => {
+                return {
+                    visible: true,
+                    visibleText: 'Hide details',
+                    hiddenText: 'Hidden tekst'
+                }
+            });
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>{this.state.visibleText}</button>
+                <p>{this.state.hiddenText}</p>
+            </div>
+        )
+    }
 }
 
-render();
+ReactDOM.render(<VisibilityToggle/>, document.getElementById('app'));
