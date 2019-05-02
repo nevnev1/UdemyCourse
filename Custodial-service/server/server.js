659b80ddd08874ac687de66bb9efa1d582d0d87f
Nevin Lenior <nevindlenior@gmail.com>
2019-05-02 16:10:28 +0200

    testing mongoose + mlab

--- Custodial-service/server/server.js
+++ Custodial-service/server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 4000;
 const dbConfig = require('./dbConfig');
+const userSchema = require('./userSchema');
 
 const uName = dbConfig.dbConfig().uName;
 const pass = dbConfig.dbConfig().pass;
@@ -11,10 +12,28 @@ const pass = dbConfig.dbConfig().pass;
 app.use(cors());
 app.use(bodyParser.json());
 
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://"+uName+":"+pass+"@custodial-service-testing-ezvqc.mongodb.net/test?retryWrites=true";
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const mongoose = require('mongoose');
+mongoose.connect(uri, { useNewUrlParser: true });
+
+const User = mongoose.model('User', userSchema);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('test');
+});
+
+User.findOne({ 'email': 'test@test.nl' }, function(err, user) {
+    if (err) {
+        return handleError(err);
+    }
+    console.log(user);
+});
+
+//const client = new MongoClient(uri, { useNewUrlParser: true });
 
 //const client = new MongoClient(uri, { useNewUrlParser: true });
 // client.connect(err => {
